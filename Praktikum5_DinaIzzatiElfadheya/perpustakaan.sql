dccad5b20357a432abd9ea02ec5bacb187a85be4
Dina Izzati Elfadheya <145301696+dinzzti@users.noreply.github.com>
2025-05-08 22:28:23 +0800

    perpustakaan.sql

--- Praktikum5_DinaIzzatiElfadheya/perpustakaan.sql
+++ Praktikum5_DinaIzzatiElfadheya/perpustakaan.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `buku` (
-  `id_buku` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `judul_buku` varchar(30) NOT NULL,
   `penulis` varchar(30) NOT NULL,
   `penerbit` varchar(30) NOT NULL,
@@ -40,9 +40,9 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `judul_buku`, `penulis`, `penerbit`, `tahun_terbit`) VALUES
-(0, 'Midnights In December', 'Castlekith', 'Bukune', 2023),
-(0, 'Chezalova', 'Moonzynne', 'Bukune', 2023),
-(0, 'Cause You Have To', 'Qlomeu', 'Bukune', 2024);
+(1, 'Midnights In December', 'Castlekith', 'Bukune', 2023),
+(2, 'Chezalova', 'Moonzynne', 'Bukune', 2023),
+(3, 'Cause You Have To', 'Qlomeu', 'Bukune', 2024);
 
 -- --------------------------------------------------------
 
@@ -51,7 +51,7 @@ INSERT INTO `buku` (`id_buku`, `judul_buku`, `penulis`, `penerbit`, `tahun_terbi
 --
 
 CREATE TABLE `member` (
-  `id_member` int(11) NOT NULL,
+  `id_member` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nama_member` varchar(50) NOT NULL,
   `nomor_member` varchar(15) NOT NULL,
   `alamat` text NOT NULL,
@@ -64,9 +64,9 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`id_member`, `nama_member`, `nomor_member`, `alamat`, `tgl_mendaftar`, `tgl_terakhir_bayar`) VALUES
-(0, 'Dina Izzati Elfadheya', '2310817120001', 'Banjarmasin', '2025-05-05', '2025-05-08'),
-(0, 'Raudatul Sholehah', '2310817210002', 'Sungai Miai', '2025-05-08', '2025-05-10'),
-(0, 'Sheila Sabina', '23108172100028', 'Sungai Ulin', '2025-05-07', '2025-05-09');
+(1, 'Dina Izzati Elfadheya', '2310817120001', 'Banjarmasin', '2025-05-05', '2025-05-08'),
+(2, 'Raudatul Sholehah', '2310817210002', 'Sungai Miai', '2025-05-08', '2025-05-10'),
+(3, 'Sheila Sabina', '23108172100028', 'Sungai Ulin', '2025-05-07', '2025-05-09');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,7 @@ INSERT INTO `member` (`id_member`, `nama_member`, `nomor_member`, `alamat`, `tgl
 --
 
 CREATE TABLE `peminjaman` (
-  `id_peminjaman` int(11) NOT NULL,
+  `id_peminjaman` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -85,9 +85,9 @@ CREATE TABLE `peminjaman` (
 --
 
 INSERT INTO `peminjaman` (`id_peminjaman`, `tgl_pinjam`, `tgl_kembali`) VALUES
-(0, '2025-05-05', '2025-05-08'),
-(0, '2025-05-08', '2025-05-12'),
-(0, '2025-05-12', '2025-05-15');
+(1, '2025-05-05', '2025-05-08'),
+(2, '2025-05-08', '2025-05-12'),
+(3, '2025-05-12', '2025-05-15');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
